326a4123cce78142cb3161676c3097558ff1f407
Yuriy Samsonov <belomore2007@gmail.com>
2016-10-06 13:09:05 +0300

    Solve lab3

---
+++ lab3/src/main/java/me/academeg/SplitSecretChina/SplitSecretChinaDecode.java
@@ -0,0 +1,71 @@
+package me.academeg.SplitSecretChina;
+
+@SuppressWarnings("unused")
+public class SplitSecretChinaDecode {
+
+    private int[] publicKey;
+    private int[] secretKey;
+    private int countSecret;
+
+    public SplitSecretChinaDecode(int[] publicKey, int[] secretKey) {
+        this.publicKey = publicKey;
+        this.secretKey = secretKey;
+        if (publicKey.length != secretKey.length) {
+            throw new IllegalArgumentException("Different keys length");
+        }
+        countSecret = publicKey.length;
+    }
+
+    public int getSecret() {
+        int[] f = generateAdditionalArr();
+        int secret = 0;
+        for (int i = 0; i < countSecret; i++) {
+            secret += secretKey[i] * getMultiplyPublicWithout(i) * f[i];
+        }
+        return secret % getMultiplyPublicWithout(-1);
+    }
+
+    private int[] generateAdditionalArr() {
+        int[] arr = new int[countSecret];
+        for (int i = 0; i < countSecret; i++) {
+            arr[i] = multiInverse(getMultiplyPublicWithout(i), publicKey[i]);
+        }
+        return arr;
+    }
+
+    private int getMultiplyPublicWithout(int withoutIndex) {
+        int res = 1;
+        for (int i = 0; i < countSecret; i++) {
+            if (i != withoutIndex) {
+                res *= publicKey[i];
+            }
+        }
+        return res;
+    }
+
+    private int multiInverse(long aa, long bb) {
+        long a = aa;
+        long b = bb;
+        long x = 0;
+        long y = 1;
+        long lastX = 1;
+        long lastY = 0;
+        long temp;
+        while (b != 0) {
+            long q = a / b;
+            long r = a % b;
+
+            a = b;
+            b = r;
+
+            temp = x;
+            x = lastX - q * x;
+            lastX = temp;
+
+            temp = y;
+            y = lastY - q * y;
+            lastY = temp;
+        }
+        return (int) ((lastX % bb + bb) % bb);
+    }
+}
---
+++ lab3/src/main/java/me/academeg/SplitSecretChina/SplitSecretChinaEncode.java
@@ -0,0 +1,68 @@
+package me.academeg.SplitSecretChina;
+
+@SuppressWarnings("unused")
+public class SplitSecretChinaEncode {
+
+    private int publicKey[];
+    private int n;
+    private int k;
+
+    private int secretKey[];
+    private int secretRange[];
+    private int secret;
+
+    public SplitSecretChinaEncode(int[] publicKey) {
+        this.publicKey = publicKey;
+        this.n = publicKey.length;
+        this.k = n / 2;
+        generateSecretRange();
+    }
+
+    public SplitSecretChinaEncode(int[] publicKey, int k) {
+        this.publicKey = publicKey;
+        this.n = publicKey.length;
+        if (k > n) {
+            throw new IllegalArgumentException("K should be greater then N");
+        }
+        this.k = k;
+        generateSecretRange();
+    }
+
+    public int[] getSecretKey() {
+        return secretKey;
+    }
+
+    private void generateSecretKey() {
+        secretKey = new int[n];
+        for (int i = 0; i < n; i++) {
+            secretKey[i] = secret % publicKey[i];
+        }
+    }
+
+    private void generateSecretRange() {
+        secretRange = new int[]{1, 1};
+        for (int i = 0; i < k; i++) {
+            secretRange[1] *= publicKey[i];
+        }
+        for (int i = 0; i < k - 1; i++) {
+            secretRange[0] *= publicKey[publicKey.length - 1 - i];
+        }
+        secret = secretRange[0] + (secretRange[1] - secretRange[0]) / 2;
+    }
+
+    public int[] getRangeSecret() {
+        return secretRange;
+    }
+
+    public int getSecret() {
+        return secret;
+    }
+
+    public void setSecret(int secret) {
+        if (!(secretRange[0] < secret && secret < secretRange[1])) {
+            throw new IllegalArgumentException("Wrong secret");
+        }
+        this.secret = secret;
+        generateSecretKey();
+    }
+}
