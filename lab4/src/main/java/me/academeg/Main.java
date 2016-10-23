bedf3bc7902691e8502fb5884ca7ab02e7492c6d
Yuriy Samsonov <belomore2007@gmail.com>
2016-10-23 19:26:56 +0300

    Print debug info and try to fix bug

--- lab4/src/main/java/me/academeg/Main.java
+++ lab4/src/main/java/me/academeg/Main.java
@@ -16,20 +16,21 @@ public class Main {
     }
 
     private void run() {
-        int p = 7;
+        int p = 23;
         int q = 11;
         int n = p * q;
 
-        int id = 1;
+        int id = 2;
 
         int a = CocksPKG.hashA(id, n);
 
         CocksEncoder encoder = new CocksEncoder();
-        String stockText = "Aa";
+        String stockText = "Aabdcrbrenr";
         int[][] encode = encoder.encode(stockText, a, n);
 
         CocksDecoder decoder = new CocksDecoder();
-        String decode = decoder.decoder(encode, CocksPKG.userR(a, p, q), a, n);
+        int r = CocksPKG.userR(a, p, q);
+        String decode = decoder.decoder(encode, r, a, n);
         System.out.println(decode);
     }
 }
